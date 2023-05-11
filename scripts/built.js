ff604715a5258d7f03a68a9b867a2a33fce18f0e
s1f10210276 <s1F102102762@iniad.org>
2023-05-11 14:25:59 +0900

    37

--- scripts/built.js
+++ scripts/built.js
@@ -1,3 +1,32 @@
+function set2fig(num) {
+  var ret;
+  if (num < 10) {
+      ret = "0" + num;
+  } else {
+      ret = num;
+  }
+  return ret;
+}
+
+function getNow() {
+  var mon = new Date().getMonth() + 1;
+  var day = new Date().getDate();
+  var you = new Date().getDay();
+  var youbi = new Array("日", "月", "火", "水", "木", "金", "土");
+  var hour = new Date().getHours();
+  var min = set2fig(new Date().getMinutes());
+  var s = mon + "月" + day + "日" + " (" + youbi[you] + ")　" + hour + ":" + min;
+  document.getElementById("view_time").innerHTML = s;
+  if (hour <= 11) {
+      document.getElementById("greet").textContent ="おはようございます。";
+  } else if (hour <= 17) {
+      document.getElementById("greet").textContent ="こんにちは。";
+  } else {
+      document.getElementById("greet").textContent ="こんばんは。";
+  }
+}
+setInterval('getNow()', 1000);
+
 function makeBasicAuth(userid, userpw) {
   let token = userid + ':' + userpw;
   let hash = btoa(token);
@@ -62,4 +91,33 @@ function callRoomStatusAPI(url, method, userid, userpw, callback) {
         }
 	    }
 	});
+}
+
+function displayRoomStatus(result) {
+  let temperature = document.getElementById('room-temperature')
+  let humidity = document.getElementById('room-humidity')
+  let airpressure = document.getElementById('room-airpressure')
+  let illuminance = document.getElementById('room-illuminance')
+  alert(result.description);
+
+  if (result.status == 'success') {
+    temperature.textContent = result.temperature;
+    humidity.textContent = result.humidity;
+    airpressure.textContent = result.airpressure;
+    illuminance.textContent = result.illuminance;
+  } else {
+    temperature.textContent = '--';
+    humidity.textContent = '--';
+    airpressure.textContent = '--';
+    illuminance.textContent = '--';
+  }
+}
+
+function getRoomStatus(roomNum) {
+  let url = 'https://edu-iot.iniad.org/api/v1' + '/sensors/' + roomNum;
+  callRoomStatusAPI(url, 'GET', userid, userpw, displayRoomStatus)
+}
+
+function cafeteria() {
+  alert('営業時間: 平日 11:00~14:00');
 }
