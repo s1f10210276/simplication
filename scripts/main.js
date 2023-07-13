ab36439a0550e6e8f11b62f7540ca2625e33e984
s1f10210276 <s1F102102762@iniad.org>
2023-07-13 17:42:50 +0900

    43

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,37 @@ function getNow() {
 }
 setInterval('getNow()', 1000);
 
+function cafeteria() {
+  alert('営業時間: 平日 11:00~14:00');
+}
+
+function displayRoomStatus(result) {
+  let temperature = document.getElementById('room-temperature')
+  let humidity = document.getElementById('room-humidity')
+  let airpressure = document.getElementById('room-airpressure')
+  let illuminance = document.getElementById('room-illuminance')
+  alert(result.description);
+
+  if (result.status == 'success') {
+    temperature.textContent = result.temperature;
+    humidity.textContent = result.humidity;
+    airpressure.textContent = result.airpressure;
+    illuminance.textContent = result.illuminance;
+  } else {
+    temperature.textContent = '--';
+    humidity.textContent = '--';
+    airpressure.textContent = '--';
+    illuminance.textContent = '--';
+  }
+}
+
+function getRoomStatus(roomNum) {
+  let userid = process.env.USER_ID
+  let userpw = process.env.USER_PW
+  let url = 'https://edu-iot.iniad.org/api/v1' + '/sensors/' + roomNum;
+  callRoomStatusAPI(url, 'GET', userid, userpw, displayRoomStatus)
+}
+
 function makeBasicAuth(userid, userpw) {
   let token = userid + ':' + userpw;
   let hash = btoa(token);
@@ -91,33 +122,4 @@ function callRoomStatusAPI(url, method, userid, userpw, callback) {
         }
 	    }
 	});
-}
-
-function displayRoomStatus(result) {
-  let temperature = document.getElementById('room-temperature')
-  let humidity = document.getElementById('room-humidity')
-  let airpressure = document.getElementById('room-airpressure')
-  let illuminance = document.getElementById('room-illuminance')
-  alert(result.description);
-
-  if (result.status == 'success') {
-    temperature.textContent = result.temperature;
-    humidity.textContent = result.humidity;
-    airpressure.textContent = result.airpressure;
-    illuminance.textContent = result.illuminance;
-  } else {
-    temperature.textContent = '--';
-    humidity.textContent = '--';
-    airpressure.textContent = '--';
-    illuminance.textContent = '--';
-  }
-}
-
-function getRoomStatus(roomNum) {
-  let url = 'https://edu-iot.iniad.org/api/v1' + '/sensors/' + roomNum;
-  callRoomStatusAPI(url, 'GET', userid, userpw, displayRoomStatus)
-}
-
-function cafeteria() {
-  alert('営業時間: 平日 11:00~14:00');
 }
